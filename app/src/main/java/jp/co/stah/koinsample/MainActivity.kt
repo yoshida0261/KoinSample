4b605c9f56148a385eaf428f5d47d6d26dde2db3
yoshida0261 <yoshida261@gmail.com>
2019-05-30 02:47:33 +0900

    feat: add inject

--- app/src/main/java/jp/co/stah/koinsample/MainActivity.kt
+++ app/src/main/java/jp/co/stah/koinsample/MainActivity.kt
@@ -2,11 +2,16 @@ package jp.co.stah.koinsample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println("_/_/ ${this.viewModel.greet()}")
     }
 }
